da17fb7e178650c80d45e4c84adfd2c40689f883
dacia-logan <grimmeeh@gmail.com>
2017-11-26 19:11:48 +0000

    lengd myndbands fall og tímisíðan bætt við

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ request.open("GET", "./videos.json", false);
 request.send(null)
 var json= JSON.parse(request.responseText);
 
-var container, flokkur, vidbox, div1, div2, div3, vid, video, a, img;
+var container, flokkur, vidbox, div1, div2, div3, vid, video, a, p, img;
 var h1, h2;
 var sida2 = 'video.html?id='
 
@@ -25,21 +25,27 @@ function titlar(obj) {
 
     for(j=0; j<json.categories[i].videos.length; j+=1){
 
+      var queryS = '?id='+json.videos[json.categories[i].videos[j] - 1].id;
+
       vidiv = document.createElement('div');
       div3 = document.createElement('div');
       a = document.createElement('a');
       img = document.createElement('img');
       h2 = document.createElement('h2');
+      p = document.createElement('p');
       div2.appendChild(vidiv);
       vidiv.appendChild(div3);
       div3.appendChild(a);
+      div3.appendChild(p);
+      div3.appendChild(document.createTextNode(lengdMyndb(json.videos[json.categories[i].videos[j] - 1].duration)));
       a.appendChild(img);
+      p.appendChild(document.createTextNode(created(json.videos[json.categories[i].videos[j] - 1].created)));
       h2.appendChild(document.createTextNode(json.videos[json.categories[i].videos[j] - 1].title));
       a.appendChild(h2);
 
       vidiv.setAttribute('class', 'myndband')
       img.src = json.videos[json.categories[i].videos[j] -1].poster;
-      a.setAttribute('href', 'videos.html'+'?id='+json.videos[json.categories[i].videos[j] - 1].id);
+      a.setAttribute('href', 'videos.html'+queryS);
     }
 
  }
@@ -48,10 +54,41 @@ function titlar(obj) {
 function created(millisekundur){
 
   var dags= new Date();
-  var created= dags-millisekundur;
+  var created = dags-millisekundur;
 
+  if((Math.floor(created/1000/60/60/24/365)>0)){
+    createdEining = "Fyrir "+(Math.floor(created/1000/60/60/24/365))+" árum síðan";
+    return createdEining;
+  }
+  else if((Math.floor(created/1000/60/60/24/30)>1)){
+    createdEining = "Fyrir "+(Math.floor(created/1000/60/60/24/30))+" mánuðum síðan";
+    return createdEining;
+  }
+  else if((Math.floor(created/1000/60/60/24/7)>1)){
+    createdEining = "Fyrir "+(Math.floor(created/1000/60/60/24/7))+" vikum síðan";
+    return createdEining;
+  }
+  else if((Math.floor(created/1000/60/60/24)>1)){
+    createdEining = "Fyrir "+(Math.floor(created/1000/60/60/24))+" dögum síðan";
+    return createdEining;
+  }
 }
 
+function lengdMyndb(sekundur) {
+
+  var min = Math.floor(sekundur/60);
+  var sek = Math.floor(sekundur%60);
+  var timi;
+
+  if(sek<10){
+    sek = '0'+sek;
+  }
+  if(min<10){
+    min = min;
+  }
+  timi = min+':'+sek;
+  return timi;
+}
 
 
   /*
